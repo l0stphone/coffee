cd1da9386e3fd956acf218e4706aaff48f0ca647
l0stphone <l0stphone@github.com>
2019-08-01 14:13:08 -0500

    no u

--- coffee.py
+++ coffee.py
@@ -1,1 +1,16 @@
+import time
+import os
+import sys
 
+def clear():
+    if(os.name == 'nt'):
+        os.system('title Coffee')
+        os.system('cls')
+    elif(os.name == 'posix'):
+        os.system('clear')
+    else:
+        print("Your Operating System is not supported by this version the game. Please try again or check the releases on GitHub or itch.")
+        time.sleep(9)
+        raise SystemExit
+
+clear()
